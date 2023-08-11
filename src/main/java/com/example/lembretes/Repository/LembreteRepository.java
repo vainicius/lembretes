dfbd171af5f630f8240c7c1b8108f7d17a436707
vainicius <comercial.viniciusgabriel@gmail.com>
2023-08-10 23:10:05 -0300

    Lembrete Service Ok!

--- src/main/java/com/example/lembretes/Repository/LembreteRepository.java
+++ src/main/java/com/example/lembretes/Repository/LembreteRepository.java
@@ -3,7 +3,12 @@ package com.example.lembretes.Repository;
 import com.example.lembretes.Entity.Lembrete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LembreteRepository extends JpaRepository<Lembrete, Long> {
+    @Query("from Lembrete where pessoa.nome Like :nome")
+    public List<Lembrete> findByNome(@Param("nome") final String nome);
 
 }
--- src/main/java/com/example/lembretes/Service/LembreteService.java
+++ src/main/java/com/example/lembretes/Service/LembreteService.java
@@ -1,7 +1,56 @@
 package com.example.lembretes.Service;
 
+import com.example.lembretes.DTO.LembreteDTO;
+import com.example.lembretes.Entity.Lembrete;
+import com.example.lembretes.Repository.LembreteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class LembreteService {
+
+    @Autowired
+    private LembreteRepository lembreteRepository;
+    public Lembrete ControleLembrete(LembreteDTO lembreteDTO){
+        Lembrete lembrete = new Lembrete();
+
+        lembrete.setId(lembreteDTO.getId());
+        lembrete.setConteudo(lembreteDTO.getConteudo());
+        lembrete.setPessoa(lembreteDTO.getPessoa());
+
+        return lembrete;
+    }
+    public Lembrete cadastrar(final LembreteDTO lembreteDTO ){
+
+        Assert.notNull(lembreteDTO.getPessoa(), "Por favor, insira uma pessoa.");
+
+        Assert.notNull(lembreteDTO.getConteudo(), "Por favor, insira um conteúdo.");
+        Assert.hasText(lembreteDTO.getConteudo(), "Conteúdo inválido.");
+
+        Lembrete lembrete = ControleLembrete(lembreteDTO);
+
+        return this.lembreteRepository.save(lembrete);
+    }
+    public Lembrete editar(final LembreteDTO lembreteDTO){
+
+        Assert.notNull(lembreteDTO.getId(), "Por favor, informe um ID.");
+        Assert.notNull(lembreteDTO.getConteudo(), "Por favor, insira um conteúdo");
+        Assert.hasText(lembreteDTO.getConteudo(), "Conteúdo inválido");
+        Assert.notNull(lembreteDTO.getPessoa(), "Pessoa não informada");
+
+        Lembrete lembrete = ControleLembrete(lembreteDTO);
+
+        return this.lembreteRepository.save(lembrete);
+    }
+    public String deletar(final Long id){
+
+        final Lembrete lembreteBanco = this.lembreteRepository.findById(id).orElse(null);
+        Assert.notNull(lembreteBanco, "Lembrete não localizado.");
+        this.lembreteRepository.delete(lembreteBanco);
+
+        return "Esse lembrete não está mais entre nós.";
+    }
+
 }
